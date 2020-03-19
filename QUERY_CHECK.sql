045f18e2ca070a7ac084ad906d7a21cc310f82cb
gustavo-exe <gus.elim2@gmail.com>
2020-03-18 22:45:42 -0600

    Checks para empelado e inventario agregados

--- QUERY_CHECK.sql
+++ QUERY_CHECK.sql
@@ -3,4 +3,13 @@ use makeupbar;
 -- - - - - - - 
 -- Checks
 -- - - - - - -
+ALTER TABLE empleado
+	add constraint CHK_Empleado_rol
+    check( rol = "Administrador" or rol = "Gerente");
 
+alter table inventario
+	add constraint CHK_Inventario_costoTotal
+    check(costoTotal > 0 );
+    
+    
+	
